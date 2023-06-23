92c9b4178ae9dd2357079cde75c4eae17eee691f
Asilsay <lamufaku@gmail.com>
2023-06-23 21:44:49 +0700

    style: slicing detail tawar produk

--- src/pages/DetailBeli.tsx
+++ src/pages/DetailBeli.tsx
@@ -16,6 +16,11 @@ const schemaConfirm = Yup.object().shape({
   quantity: Yup.number().required('Required'),
 });
 
+const schemaTawar = Yup.object().shape({
+  nego: Yup.string().required('Required'),
+  quantity: Yup.string().required('Required'),
+});
+
 function DetailBeli() {
   const [cookie] = useCookies(['role']);
   const ckRole = cookie.role;
@@ -34,9 +39,21 @@ function DetailBeli() {
       console.log(values);
     },
   });
+  const formikTawar = useFormik({
+    initialValues: {
+      nego: '',
+      quantity: '',
+    },
+    validationSchema: schemaTawar,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
 
   useEffect(() => {
     console.log(type, productId);
+    formikTawar.setFieldValue('nego', dummyData.price);
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -121,45 +138,125 @@ function DetailBeli() {
           </div>
         </form>
       </Modals>
-      <Layout
-        chose="section"
-        addClass="w-full min-h-screen -mt-[67px] xl:-mt-[64px]  px-16 py-16  flex items-center "
-      >
-        <div className="w-full h-full flex md:flex-row flex-col justify-center items-center pt-8 ">
-          <div className="basis-full  md:basis-1/2 flex justify-end   mb-7 md:mr-7">
-            <div className="bg-slate-500 h-max rounded-2xl">
-              <img
-                src={imgBwh}
-                alt="gabung"
-                className="bg-cover bg-center rounded-2xl h-full w-full md:h-80 md:w-80 lg:h-[28rem] lg:w-[28rem]"
-              />
-            </div>
-          </div>
 
-          <div className="basis-full md:basis-1/2  flex flex-col gap-4 p-3 h-full md:h-80  lg:h-[28rem] justify-between">
-            <div className="">
-              <p className="text-3xl font-bold tracking-wider ">
-                {dummyData.name}{' '}
-              </p>
-              <p className="text-xl my-5">{dummyData.description}</p>
-              <p className="text-xl">
-                Harga: <span className="font-semibold">{dummyData.price}</span>{' '}
-              </p>
-              <p className="text-xl">
-                Stok: <span className="font-semibold">{dummyData.stock}</span>{' '}
-              </p>
+      {type === 'alat' ? (
+        <Layout
+          chose="section"
+          addClass="w-full min-h-screen -mt-[67px] xl:-mt-[64px]  px-16 py-16  flex items-center "
+        >
+          <div className="w-full h-full flex md:flex-row flex-col justify-center items-center pt-8 ">
+            <div className="basis-full  md:basis-1/2 flex justify-end   mb-7 md:mr-7">
+              <div className="bg-slate-500 h-max rounded-2xl">
+                <img
+                  src={imgBwh}
+                  alt="gabung"
+                  className="bg-cover bg-center rounded-2xl h-full w-full md:h-80 md:w-80 lg:h-[28rem] lg:w-[28rem]"
+                />
+              </div>
             </div>
 
-            <label
-              id="button-to-beli"
-              htmlFor="modal-buy"
-              className="btn btn-primary text-base text-primary-content w-max self-end"
-            >
-              Beli Sekarang
-            </label>
+            <div className="basis-full md:basis-1/2  flex flex-col gap-4 p-3 h-full md:h-80  lg:h-[28rem] justify-between">
+              <div className="">
+                <p className="text-3xl font-bold tracking-wider ">
+                  {dummyData.name}{' '}
+                </p>
+                <p className="text-xl my-5">{dummyData.description}</p>
+                <p className="text-xl">
+                  Harga:{' '}
+                  <span className="font-semibold">{dummyData.price}</span>{' '}
+                </p>
+                <p className="text-xl">
+                  Stok: <span className="font-semibold">{dummyData.stock}</span>{' '}
+                </p>
+              </div>
+
+              <label
+                id="button-to-beli"
+                htmlFor="modal-buy"
+                className="btn btn-primary text-base text-primary-content w-max self-end"
+              >
+                Beli Sekarang
+              </label>
+            </div>
           </div>
-        </div>
-      </Layout>
+        </Layout>
+      ) : (
+        <Layout
+          chose="section"
+          addClass="w-full min-h-screen -mt-[67px] xl:-mt-[64px]  px-16 py-16  flex items-center "
+        >
+          <div className="w-full h-full flex md:flex-row flex-col justify-center items-center pt-8 ">
+            <div className="basis-full  md:basis-1/2 flex justify-end   mb-7 md:mr-7">
+              <div className="bg-slate-500 h-max rounded-2xl">
+                <img
+                  src={imgBwh}
+                  alt="gabung"
+                  className="bg-cover bg-center rounded-2xl h-full w-full md:h-80 md:w-80 lg:h-[28rem] lg:w-[28rem]"
+                />
+              </div>
+            </div>
+
+            <div className="basis-full md:basis-1/2  flex flex-col gap-4 p-3 h-full md:h-80  lg:h-[28rem] justify-between">
+              <div className="">
+                <p className="text-3xl font-bold tracking-wider ">
+                  {dummyData.name}{' '}
+                </p>
+                <p className="text-xl my-5">{dummyData.description}</p>
+                <p className="text-xl">
+                  Harga:{' '}
+                  <span className="font-semibold">{dummyData.price}</span>{' '}
+                </p>
+                <p className="text-xl">
+                  Stok: <span className="font-semibold">{dummyData.stock}</span>{' '}
+                </p>
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <p className="text-lg font-semibold">Nego Harga:</p>
+                <form
+                  onSubmit={formikTawar.handleSubmit}
+                  className="w-full flex justify-between"
+                >
+                  <div className="flex w-3/4 gap-3">
+                    <div className="w-2/3">
+                      <Input
+                        id="nego"
+                        name="nego"
+                        label="input harga nego"
+                        type="number"
+                        value={formikTawar.values.nego}
+                        onChange={formikTawar.handleChange}
+                        onBlur={formikTawar.handleBlur}
+                        error={formikTawar.errors.nego}
+                        touch={formikTawar.touched.nego}
+                      />
+                    </div>
+                    <div className="w-1/3">
+                      <Input
+                        id="quantity"
+                        name="quantity"
+                        label="jumlah"
+                        type="number"
+                        value={formikTawar.values.quantity}
+                        onChange={formikTawar.handleChange}
+                        onBlur={formikTawar.handleBlur}
+                        error={formikTawar.errors.quantity}
+                        touch={formikTawar.touched.quantity}
+                      />
+                    </div>
+                  </div>
+                  <button
+                    id="button-to-tawar"
+                    className="btn btn-primary text-base text-primary-content w-max"
+                  >
+                    Tawar
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      )}
     </Layout>
   );
 }
